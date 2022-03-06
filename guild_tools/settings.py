3ffecac50cead51bffc4272809b6d141c56948ec
Luiz Duarte <luiz.duarte@vittude.com>
2022-03-06 20:33:13 -0300

    update ALLOWED_HOSTS

--- guild_tools/settings.py
+++ guild_tools/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6zlywxy2%&p-a6crhxhks+*61sd^v(*%t%_l)co*_002z&bfk%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
